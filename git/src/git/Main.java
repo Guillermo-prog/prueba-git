1bb7fa8674e9aa73580c723a39e5355cf6f575dd
guill <guill@MSI>
2020-10-06 10:31:35 +0200

    Estructura inicial del proyecto

---
+++ git/src/git/Main.java
@@ -0,0 +1,21 @@
+package git;
+
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("Hola mundo!");
+	
+	}
+	
+	/*SISTEMA DE CONTROL DE VERSIONES
+	 * 
+	 * scv DISTRIBUIDO:
+	 * 
+	 * -repositorio de codigo local: cada persona tiene uno, y 
+	 * -repositorio remoto: donde se pueden subir los cambios del repositorio local, y bajar los cambios de
+	 * otra persona. Las empresas pueden tener el suyo propio, o puedes contratar repositorios remotos
+	 * de empresas como GitHub o GitLab.
+	 * 
+	 */
+
+
+}
